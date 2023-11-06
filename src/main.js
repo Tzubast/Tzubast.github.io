5c44eb51e8b9360d25f0e408be477c0aa2756b86
Tzubast <121686868+Tzubast@users.noreply.github.com>
2023-11-07 01:27:04 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
-import './assets/main.css'
-import './assets/base.css'
-import './views/Calc.vue'
-
-import App from './App.vue'
-import router from './router/index.js'
-
 const app = createApp(App)
 
 app.use(router)
